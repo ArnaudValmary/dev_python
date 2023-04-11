02ba121106571325c55e51bd4a68eeaa82c38d70
Arnaud Valmary <github@valmary.eu>
2023-04-11 17:49:22 +0200

    Fix variable names is coverage script ('run_coverage.sh')

--- run_coverage.sh
+++ run_coverage.sh
@@ -4,8 +4,8 @@
 
 declare -a coverage_run_with_pytest_and_build_xml_report=(
     "pytest"
-    "--cov-config=${coveragerc_filename}"
-    "--cov-report" "xml:${coveragerc_xml_report_filename}"
+    "--cov-config=${coverage_config_filename}"
+    "--cov-report" "xml:${coverage_xml_report_filename}"
 )
 for sources_dir in "${sources_dirs[@]}"; do
     coverage_run_with_pytest_and_build_xml_report+=("--cov")
@@ -20,15 +20,15 @@ declare -a coverage_build_html_report=(
 run_coverage_and_build_xml_report () {
     export_vars
     set -x
-    rm -f "${coveragerc_report_filename}"
-    rm -f "${coveragerc_xml_report_filename}"
+    rm -f "${coverage_report_filename}"
+    rm -f "${coverage_xml_report_filename}"
     "${pipenv_run[@]}" "${coverage_run_with_pytest_and_build_xml_report[@]}"
     set +x
 }
 run_build_coverage_html_report () {
     export_vars
     set -x
-    rm -rf "${coveragerc_html_report_dirname}"
+    rm -rf "${coverage_html_report_dirname}"
     "${pipenv_run[@]}" "${coverage_build_html_report[@]}"
     set +x
 }
